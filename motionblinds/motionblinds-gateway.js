6c9cfb62845abab4e240e5f9d10f6f8eeff090f3
1voud <johan@bartox.nl>
2023-12-09 11:50:47 +0100

    First working release

--- motionblinds/motionblinds-gateway.js
+++ motionblinds/motionblinds-gateway.js
@@ -1,18 +1,18 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
 
     var mb = require('motionblinds');
 
     function MotionBlindsGateway(n) {
 
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         var node = this;
         this.apikey = n.apikey;
 
         this.gw = new mb.MotionGateway({ key: this.apikey });
-        console.log(this.gw);
+        // console.log(this.gw);
 
         this.gw.start();
-        
+
         node.status('MotionBlindsGateway Started');
         this.gw.readAllDevices().then((result) => {
             var globalContext = this.context().global;
@@ -21,11 +21,11 @@ module.exports = function(RED) {
         })
 
 
-        node.on("close", function() {
+        node.on("close", function () {
             gw.stop();
             node.status('MotionBlindsGateway Stopped');
         });
     }
-    RED.nodes.registerType("motionblinds-gateway",MotionBlindsGateway);
+    RED.nodes.registerType("motionblinds-gateway", MotionBlindsGateway);
 }
 
--- motionblinds/motionblinds.js
+++ motionblinds/motionblinds.js
@@ -1,33 +1,43 @@
-module.exports = function(RED) {
-    
+module.exports = function (RED) {
+
     var mb = require('motionblinds');
 
     function MotionBlinds(config) {
 
-        RED.nodes.createNode(this,config);
-        
+        RED.nodes.createNode(this, config);
+
         var node = this;
 
-        this.gateway = RED.nodes.getNode(config.gateway);    
+        this.gateway = RED.nodes.getNode(config.gateway);
         const gw = this.gateway.gw;
 
         gw.on('report', (report) => {
             let msg = {};
             msg.payload = report;
-            console.log(msg);
+
+            var globalContext = this.context().global;
+            blinds = globalContext.get("motion-blinds")
+            blinds.forEach((blind) => {
+                if (blind.mac === mac) {
+                    blind.data = report.data;
+                    blind.msgType = report.msgType;
+                    blind.msgID = report.msgID;
+                }
+            });
+
             node.send(msg);
         });
 
-        node.on('input', function(msg) {
+        node.on('input', function (msg) {
             value = msg.payload.value;
             mac = msg.payload.mac;
             let data = {};
-            switch(msg.payload.action) {
+            switch (msg.payload.action) {
                 case "targetPosition":
                     data["targetPosition"] = value;
                     break;
                 case "operation":
-                    switch(value) {
+                    switch (value) {
                         case "CloseDown":
                             data["operation"] = 0;
                             break;
@@ -36,32 +46,40 @@ module.exports = function(RED) {
                             break;
                         case "Stop":
                             data["operation"] = 2;
-                            break;                                          
+                            break;
                         case "StatusQuery":
                             data["operation"] = 5;
-                            break; 
+                            break;
                     }
                     break;
                 default:
                     return;
-              }
+            }
+            var globalContext = this.context().global;
+            blinds = globalContext.get("motion-blinds")
+            blinds.forEach((blind) => {
+                if (blind.mac === mac) {
+                    gw.writeDevice(mac, blind.deviceType, data).then((ack) => {
+                        let msg = {};
+                        msg.payload = ack;
+                        blind.data = ack.data;
+                        blind.msgType = ack.msgType;
+                        blind.msgID = ack.msgID;
+                        node.send(msg);
+                    });
+                }
+            });
 
 
-              gw.writeDevice(mac, "10000000", data
-            ).then((ack) => {
-                let msg = {};
-                msg.payload = ack;
-                node.send(msg);
-            })
         });
 
 
-        node.on("close", function() {
+        node.on("close", function () {
             gw.stop();
             console.log('GW stopped');
         });
 
     }
 
-    RED.nodes.registerType("motionblinds",MotionBlinds);
+    RED.nodes.registerType("motionblinds", MotionBlinds);
 }
