9fbbb93a55ef4291544b423e2e9474a419733d23
1voud <johan@bartox.nl>
2023-12-10 20:46:51 +0100

    Catch errors

--- motionblinds/motionblinds.js
+++ motionblinds/motionblinds.js
@@ -14,62 +14,68 @@ module.exports = function (RED) {
         gw.on('report', (report) => {
             let msg = {};
             msg.payload = report;
-
-            var globalContext = this.context().global;
-            blinds = globalContext.get("motion-blinds")
-            blinds.forEach((blind) => {
-                if (blind.mac === mac) {
-                    blind.data = report.data;
-                    blind.msgType = report.msgType;
-                    blind.msgID = report.msgID;
-                }
-            });
+            try {
+                var globalContext = this.context().global;
+                blinds = globalContext.get("motion-blinds")
+                blinds.forEach((blind) => {
+                    if (blind.mac === report.mac) {
+                        blind.data = report.data;
+                        blind.msgType = report.msgType;
+                        blind.msgID = report.msgID;
+                    }
+                });
+            } catch (error) {
+                node.error(error);
+            } 
 
             node.send(msg);
         });
 
         node.on('input', function (msg) {
-            value = msg.payload.value;
-            mac = msg.payload.mac;
-            let data = {};
-            switch (msg.payload.action) {
-                case "targetPosition":
-                    data["targetPosition"] = value;
-                    break;
-                case "operation":
-                    switch (value) {
-                        case "CloseDown":
-                            data["operation"] = 0;
-                            break;
-                        case "OpenUp":
-                            data["operation"] = 1;
-                            break;
-                        case "Stop":
-                            data["operation"] = 2;
-                            break;
-                        case "StatusQuery":
-                            data["operation"] = 5;
-                            break;
-                    }
-                    break;
-                default:
+            try {
+                value = msg.payload.value;
+                mac = msg.payload.mac;
+                let data = {};
+                switch (msg.payload.action) {
+                    case "targetPosition":
+                        data["targetPosition"] = value;
+                        break;
+                    case "operation":
+                        switch (value) {
+                            case "CloseDown":
+                                data["operation"] = 0;
+                                break;
+                            case "OpenUp":
+                                data["operation"] = 1;
+                                break;
+                            case "Stop":
+                                data["operation"] = 2;
+                                break;
+                            case "StatusQuery":
+                                data["operation"] = 5;
+                                break;
+                        }
+                        break;
+                    default:
                     return;
-            }
-            var globalContext = this.context().global;
-            blinds = globalContext.get("motion-blinds")
-            blinds.forEach((blind) => {
-                if (blind.mac === mac) {
-                    gw.writeDevice(mac, blind.deviceType, data).then((ack) => {
-                        let msg = {};
-                        msg.payload = ack;
-                        blind.data = ack.data;
-                        blind.msgType = ack.msgType;
-                        blind.msgID = ack.msgID;
-                        node.send(msg);
-                    });
                 }
-            });
-
+                var globalContext = this.context().global;
+                blinds = globalContext.get("motion-blinds") || {};
+                blinds.forEach((blind) => {
+                    if (blind.mac === mac) {
+                        gw.writeDevice(mac, blind.deviceType, data).then((ack) => {
+                            let msg = {};
+                            msg.payload = ack;
+                            blind.data = ack.data;
+                            blind.msgType = ack.msgType;
+                            blind.msgID = ack.msgID;
+                            node.send(msg);
+                        });
+                    }
+                });
+            } catch (error) {
+                node.error(error);
+            }
 
         });
 
